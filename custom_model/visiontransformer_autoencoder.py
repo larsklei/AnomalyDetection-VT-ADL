f71d6bebf2b6c4e23805cf2f0d1c311c66825ad6
larsklei <lars.kleinemeier@posteo.de>
2023-11-05 18:17:00 +0100

    Added Registers functionality to the VisionAutoEncoder

--- custom_model/visiontransformer_autoencoder.py
+++ custom_model/visiontransformer_autoencoder.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import keras_core as keras
 
 from custom_model.vision_transformer import PatchEmbedding
@@ -10,32 +11,49 @@ class VisionTransformerEncoder(keras.Model):
 		decoder:
 		latent_dim:
 		patch_size:
+		num_register:
 	"""
-	def __init__(self, encoder, decoder, latent_dim, patch_size):
-		self.latent_dim = latent_dim
+	def __init__(self, emb_dim, patch_size, encoder=None, decoder=None, num_register=None):
+		self.embedding = None
+		self.emb_dim = emb_dim
 		self.patch_size = patch_size
 		self.encoder = encoder
 		self.decoder = decoder
+		self.num_register = num_register
 
 	def build(self, input_shape):
 		batch_size, img_height, img_width, channels = input_shape
 
-		self.embedding = PatchEmbedding(self.latent_dim, self.patch_size, img_height, img_width, name='Embedding')
+		self.embedding = PatchEmbedding(self.emb_dim, self.patch_size, img_height, img_width, name='Embedding')
 
-		if self.encoder == None:
+		if self.encoder is None:
 			pass
 
-		if self.decoder == None:
+		if self.decoder is None:
 			pass
 
 	def train_step(self, images):
+		batch_size = images.shape[0]
+
 		with tf.GradientTape() as tape:
 			patches = images
 			embedded_patches = self.embedding(patches)
-			encoded_patches = self.encoder(patches)
-			reconstruction = self.decoder(encoded_patches)
 
-			reconstruction_loss = VisionLoss(data, reconstruction)
+			if self.num_register is not None:
+				register_tokens = tf.random.normal(shape=[batch_size, self.num_register, self.emb_dim])
+				embedded_patches = tf.concat(embedded_patches, register_tokens)
+
+			encoded_patches = self.encoder(embedded_patches)
+
+			encoded_patches_wo_registers = encoded_patches[:, :-self.num_register, :]
+
+			representation = keras.layers.LayerNormalization()(encoded_patches_wo_registers)
+			representation = keras.layers.Flatten()(representation)
+			representation = keras.layers.Dropout(0.5)(representation)
+
+			reconstruction = self.decoder(representation)
+
+			reconstruction_loss = VisionLoss(images, reconstruction)
 			grads = tape.gradient(reconstruction_loss, self.trainable_weights)
 			self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
 			self.reconstruction_loss_tracker.update_state(reconstruction_loss)
