76005ab617e3543dd3e4a56905e824e950a3ad1e
larsklei <lars.kleinemeier@posteo.de>
2023-11-06 07:43:20 +0100

    Created test files for the custom models and layers

---
+++ custom_model/test_visiontransformer_autoencoder.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+class test_VisionTransformerAutoencoder(tf.test.TestCase):
+	def setUp(self):
+		pass
+
+	def test_VisionTransformerAutoencoder_Output(self):
+		pass
---
+++ custom_model/test_visiontransformer_layers.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+class PatchEmbeddingTest(tf.test.TestCase):
+	def setUp(self):
+		pass
+
+	def test_PatchEmbeddingOutput(self):
+		pass
