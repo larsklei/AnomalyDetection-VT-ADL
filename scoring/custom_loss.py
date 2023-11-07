7f53b302e6bbb5edfb78caf486eb8c5a3b446f18
larsklei <lars.kleinemeier@posteo.de>
2023-11-07 08:42:56 +0100

    updated and simplified the loss function

--- scoring/custom_loss.py
+++ scoring/custom_loss.py
@@ -4,33 +4,10 @@ import keras_core as keras
 from tensorflow.keras.losses import MeanSquaredError, Loss
 from tensorflow.image import ssim
 
-
-def StructuralSimilarityIndex(y_true, y_pred):
-	r"""
-	A wrapper for the ssim function as a Keras Loss function.
-	"""
+def loss_vision(y_true, y_pred):
 	ssim_value = ssim(y_true, y_pred, max_val=1.0)
-	loss = 1 - tf.reduce_mean(ssim_value)
+	ssim_loss = 1 - tf.reduce_mean(ssim_value)
 
-	return loss
+	mse_loss = MeanSquaredError()(y_true, y_pred)
 
-
-class VisionLoss(Loss):
-	r"""
-
-
-	Args:
-		lambda1:
-		lambda2:
-	"""
-
-	def __init__(self, lambda1=5, lambda2=0.5, name=None):
-		super().__init__(VisionLoss, name=name)
-		self.lambda1 = lambda1
-		self.lambda2 = lambda2
-
-	def call(self, y_true, y_pred):
-		mse = MeanSquaredError()
-		ssim_loss = StructuralSimilarityIndex()
-
-		return self.lambda1 * mse(y_true, y_pred) + self.lambda2 * ssim_loss(y_true, y_pred)
+	return 5*mse_loss+0.5*ssim_loss
