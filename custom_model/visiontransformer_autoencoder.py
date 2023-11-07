9500aed502a2d831cfbee3774701e0fa45dd02c1
larsklei <lars.kleinemeier@posteo.de>
2023-11-07 09:18:24 +0100

    fixed smaller typos.

--- custom_model/visiontransformer_autoencoder.py
+++ custom_model/visiontransformer_autoencoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import keras_core as keras
 
 from custom_model.visiontransformer_layers import PatchEmbedding, VisionTransformerEncoder, get_decoder
-from scoring.custom_loss import VisionLoss
+from scoring.custom_loss import loss_vision
 
 class VisionTransformerAutoencoder(keras.Model):
 	r"""
@@ -31,6 +31,7 @@ class VisionTransformerAutoencoder(keras.Model):
 		
 	def call(self, inputs, training=False):
 		batch_size = inputs.shape[0]
+		
 		patches = inputs
 		embedded_patches = self.embedding(patches)
 		
@@ -43,7 +44,6 @@ class VisionTransformerAutoencoder(keras.Model):
 			encoded_patches_wo_registers = self.encoder(embedded_patches)
 		
 		representation = keras.layers.LayerNormalization()(encoded_patches_wo_registers)
-		representation = keras.layers.Dropout(0.5)(representation)
 		
 		representation = keras.layers.Reshape(
 			target_shape=(self.num_patches_root, self.num_patches_root, self.latent_dim)
@@ -65,38 +65,10 @@ class VisionTransformerAutoencoder(keras.Model):
 			
 			if reconstruction.shape[1:] != images.shape[1:]:
 				raise ValueError('Decoder is wrong. It does not reproduce the correct shape.')
-			print('Reconstruction shape: ', str(reconstruction.shape))
-			reconstruction_loss = VisionLoss(images, reconstruction)
-			print('Vision loss: ', str(reconstruction_loss))
+			reconstruction_loss = loss_vision(images, reconstruction)
 		grads = tape.gradient(reconstruction_loss, self.trainable_weights)
-		print('Max of gradient: %.4f' % tf.reduce_max(grads[0]))
 		self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
 		self.reconstruction_loss_tracker.update_state(reconstruction_loss)
 		return {
 				'reconstruction_loss': self.reconstruction_loss_tracker.result()
 			}
-	
-
-if __name__ == '__main__':
-	decoder = get_decoder()
-	encoder = keras.Sequential(
-		[
-			VisionTransformerEncoder(num_heads=8, key_dim=512, hidden_units=512, num_outputs=512),
-			VisionTransformerEncoder(num_heads=8, key_dim=512, hidden_units=512, num_outputs=512)
-		],
-		name='VisionTransformerEncoder'
-	)
-	emb = PatchEmbedding(latent_dim=512, patch_size=16, img_height=512, img_width=512)
-	
-	#ViT = VisionTransformerAutoencoder(latent_dim=512, patch_size=16, encoder=encoder, decoder=decoder, img_height=512, img_width=512)
-	
-	#ViT.compile(
-	#	optimizer=keras.optimizers.Adam(),
-	#	run_eagerly=True
-	#)
-	
-	#dummy_data = tf.ones((6, 512, 512, 3))
-	
-	#ViT.fit(dummy_data)
-	
-	#ViT.summary()
